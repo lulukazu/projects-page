5e9e6105355f0acca4a780339bf0c9102a4fb708
lulukazu <lulukazu@gmail.com>
2016-08-06 11:55:48 -0400

    pop out sub nav for each category

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav_physics() {
+    document.getElementById("physics-nav").style.left = "0px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav_physics() {
+    document.getElementById("physics-nav").style.left = "500px";
+}
