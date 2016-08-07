011471040b430f87a1c47885ce6572072f22fbf7
lulukazu <lulukazu@gmail.com>
2016-08-07 13:44:54 -0400

    more advanced scripting of contents

--- js/main.js
+++ js/main.js
@@ -45,6 +45,7 @@ function wrangleData(itemID){
 }
 
 
+
 function updateFiles(parentID,data,itemID) {
 
     lastDisp=.8;
@@ -62,13 +63,58 @@ function updateFiles(parentID,data,itemID) {
         .attr("style",function(d){
             var randRange=80;
             var leftDisp=Math.random();
-            while (Math.abs(lastDisp-leftDisp)<0.5){
+            while (Math.abs(lastDisp-leftDisp)<0.4){
                 leftDisp=Math.random();
             }
             lastDisp=leftDisp;
 
             return "left:"+leftDisp*randRange+"px; top:"+d.num*40+"px;";
         });
+
+    var links=divs.append("a")
+        .attr("href",function(d){return d.link;})
+        .attr("target","new");
+
+    links.append("div")
+        .attr("class","dot")
+        .on("mouseover",function(d){
+            var w= d.r*2;
+            var h= d.r*2;
+            var w2=w*1.2;
+
+            var t1= d.r/2;
+            var offsets =$(this).offset();
+            var top = offsets.top- d.r;
+            var left = offsets.left- d.r;
+            d3.select(this)
+                .style("background", "url("+ d.img+") no-repeat")
+                .style("background-size",w2+"px")
+                .transition()
+                .duration(400)
+                .style("width", w+"px")
+                .style("height",w+"px")
+                .style("border-radius", d.r+"px")
+                .style("left","-"+t1+"px")
+                .style("top","-"+t1+"px");
+
+        })
+        .on("mouseout",function(d){
+            var w= d.r*2;
+            var h= d.r*2;
+
+            var t1= d.r/2;
+
+            d3.select(this)
+                .transition()
+                .duration(300)
+                .style("background", "#ccc")
+                .style("width", "20px")
+                .style("height","20px")
+                .style("border-radius","10px")
+                .style("left","0px")
+                .style("top","0px");
+        });
+
 
     divs.append("img")
         .attr("src",function(d){return d.img;})
@@ -82,145 +128,33 @@ function updateFiles(parentID,data,itemID) {
         })
         .attr("class","node-text col-sm-7")
 
+
+
+    //modal javascript
+
+    var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    };
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
 }
-
-
-
-//
-////file navigation -- I decided not to go w/ svg after all :(
-//
-//ForceDiagram = function(_parentElement, _data1){
-//    this.parentElement = _parentElement;
-//    this.data = _data1;
-//    this.displayData = []; // see dataForceLayout wrangling
-//    this.initVis();
-//};
-//
-//
-//ForceDiagram.prototype.initVis = function(){
-//    var vis = this;
-//
-//    vis.Width=600;
-//    vis.Height=600;
-//    // select SVG drawing area
-//    vis.svg = d3.select("#" + vis.parentElement);
-//
-//
-//    // 1) INITIALIZE FORCE-LAYOUT
-//
-//    vis.force=d3.layout.force()
-//        .size([vis.Width,vis.Height])
-//        .charge(100)
-//        .chargeDistance(1000)
-//        .gravity(0.4)
-//        .linkDistance(300)
-//        .linkStrength(.6)
-//        .friction(.1);
-//
-//
-//    vis.wrangleData();
-//};
-//
-//
-//ForceDiagram.prototype.wrangleData = function(){
-//    var vis = this;
-//    // Update the visualization
-//    vis.updateVis();
-//};
-//
-//ForceDiagram.prototype.updateVis = function(){
-//    var vis = this;
-//
-//    // 2a) DEFINE 'NODES' AND 'EDGES'
-//    vis.force
-//        .nodes(vis.data.nodes,function(d){return d.id;})
-//        .links(vis.data.links);
-//
-//    // 2b) START RUNNING THE SIMULATION
-//    vis.force.start();
-//
-//
-//    // 3) DRAW THE LINKS (SVG LINE) ... or don't?
-//
-//    vis.link=vis.svg.selectAll(".link")
-//        .data(vis.data.links);
-//
-//    vis.link.exit().remove();
-//
-//    vis.link.enter().append("line")
-//        .attr("class","link")
-//        .attr("stroke","#bbb")
-//        .attr("stroke-width",1);
-//
-//    // 4) DRAW THE NODES (SVG CIRCLE)
-//    vis.node = vis.svg.selectAll(".node")
-//        .data(vis.data.nodes,function(d){return d.id;});
-//
-//    vis.node.exit().remove();
-//
-//    //var g_nodes=vis.node.enter().append("g")
-//    //    .attr("class", "node")
-//    //    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-//    //
-//    //g_nodes.append("circle")
-//    //    .attr("r", function(d) {return d.r; })
-//    //    .style("fill", "#eee");
-//
-//    var nodesHandle=vis.node.enter().append("g")
-//        .attr("class","node")
-//        .attr("transform",function(d) {return "translate(" + d.x + "," + d.y + ")";});
-//
-//    //nodesHandle.append("circle")
-//    //    .attr("fill","none")
-//    //    .attr("stroke","black")
-//    //    .attr("stroke-width",2)
-//    //    .attr("r",function(d){return d.r;});
-//
-//    nodesHandle.append("image")
-//        .attr("xlink:href",  function(d) { return d.img;})
-//        .attr("x", function(d){return -d.r;})
-//        .attr("y", function(d){return -d.r;})
-//        .attr("height", function(d){return 2* d.r;})
-//        .attr("width", function(d){return 2* d.r;});
-//    //
-//    //vis.node.enter().append("image")
-//    //    .attr("class","node")
-//    //    .attr("height",50)
-//    //    .attr("width",50)
-//    //    .attr("xlink:href",  function(d) { return d.img;});
-//    //
-//    //circles.append("image")
-//    //    .attr("xlink:href",  function(d) { return d.img;})
-//    //    .attr("x", function(d) { return -25;})
-//    //    .attr("y", function(d) { return -25;})
-//    //    .attr("height", 50)
-//    //    .attr("width", 50);
-//
-//
-//
-//
-//    // 5) LISTEN TO THE 'TICK' EVENT AND UPDATE THE X/Y COORDINATES FOR ALL ELEMENTS
-//
-//    vis.force.on("tick",function(){
-//        //update node coordinates
-//        vis.node
-//            .attr("cx",function(d){ return d.x; })
-//            .attr("cy",function(d){ return d.y; });
-//
-//        vis.node
-//            .attr("transform",function(d) {return "translate(" + d.x + "," + d.y + ")";});
-//
-//        //update edge coordinates
-//        vis.link
-//            .attr("x1", function(d) { return d.source.x; })
-//            .attr("y1", function(d) { return d.source.y; })
-//            .attr("x2", function(d) { return d.target.x; })
-//            .attr("y2", function(d) { return d.target.y; });
-//    });
-//
-//
-//    vis.node.call(vis.force.drag);
-//
-//};
-//
 
